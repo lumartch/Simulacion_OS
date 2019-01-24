544de7f2ce7fb95a73b708e28678c6b091af0aa8
Lumart Chryssomallis <lumartch@gmail.com>
2019-01-24 10:09:13 -0600

    Se creo la interfaz a usar, aún falta crear el método para sobreescribir en pantalla

--- interfaz.cpp
+++ interfaz.cpp
@@ -1,9 +1,65 @@
 #include "interfaz.h"
 
 Interfaz::Interfaz() {
-    //ctor
+    menuInicio();
     }
 
 Interfaz::~Interfaz() {
     //dtor
     }
+
+void Interfaz::menuInicio() {
+    string cadena;
+    do{
+        system(CLEAR);
+        cout << "Ingrese la cantidad de lotes a procesar: ";
+        getline(cin, cadena);
+        if(!checkNumero(cadena)){ break; }
+        cout << endl << endl << "Ha introducido un número o carácter inválido. Intente de nuevo.";
+        pausa();
+    }while(true);
+    procesamientoLotes();
+    proceso();
+
+    pausa();
+    }
+
+void Interfaz::procesamientoLotes() {
+    cout << "+-----------------------+-------+----------+---------+" << endl;
+    cout << "| No. Lotes Pendientes  |       | Contador |---------|" << endl;
+    cout << "+-------------+---------+-------+----------+---------+" << endl;
+    cout << "| Lote actual |         | "<< endl;
+    cout << "+-------------+---------+--------+-----------------------------------------------+---------------------------------------------------------------+" << endl;
+    cout << "|       Nombre          | Tiempo |                Proceso                        |                            Terminados                         |" << endl;
+    cout << "+-----------------------+--------+-----------------+-----------------------------+------+----------------------------+---------------------------+" << endl;
+    cout << "| Idelfonso Castellanos |        | Nombre          | Idelfonso Castellanos       | ID   |       Operación            |          Resultado        |" << endl;
+    cout << "+--------------------------------| Operación       | 9,223,372,036,854,775,807*1 +------+----------------------------+---------------------------+" << endl;
+    cout << "                                 | Tiempo          |-----------------------------|------|9,223,372,036,854,775,807*1 | 9,223,372,036,854,775,807 |" << endl;
+    cout << "                                 | T. Transcurrido |-----------------------------+------+----------------------------+---------------------------+" << endl;
+    cout << "                                 | T. Restante     |-----------------------------|" << endl;
+    cout << "                                 +-----------------+-----------------------------+" << endl;
+    }
+
+void Interfaz::proceso() {
+
+    }
+
+void Interfaz::contador() {
+    printf("\033[%d;%dH%s\n", 3, 3, "Hola mundo");
+    }
+
+
+void Interfaz::pausa(){
+    cout << endl << endl << "Presione [enter] para continuar...";
+    cin.get();
+    }
+
+bool Interfaz::checkNumero(const string& cadena) {
+    regex rx("[0-9]*");
+    if(!regex_match(cadena, rx)){
+        return true;
+    }
+    return false;
+    }
+
+
--- interfaz.h
+++ interfaz.h
@@ -1,12 +1,36 @@
 #ifndef INTERFAZ_H
 #define INTERFAZ_H
 
+#ifndef WIN_32
+#define CLEAR "clear"
+#else
+#define CLEAR "cls"
+#endif // WIN_32
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <queue>
+#include <regex>
+#include <ncurses.h>
+
+using namespace std;
 
 class Interfaz {
     public:
         Interfaz();
         virtual ~Interfaz();
+        void menuInicio();
+        void procesamientoLotes();
+
     private:
+        int cantLotes;
+        int tiempoTotal;
+        void proceso();
+        void contador();
+        void pausa();
+        bool checkNumero(const string &cadena);
+
     };
 
 #endif // INTERFAZ_H
